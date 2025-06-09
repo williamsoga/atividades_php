4ea8f42c2da4771eb46edec05f781b8cc7d22ffa
williamsoga <williamhsoga@gmail.com>
2025-06-09 10:58:14 -0300

    first commit

---
+++ atividade.php
@@ -0,0 +1,23 @@
+<?php 
+// comentario de linha com barra dupla
+#comentario  de linha com sequilha
+
+/* comentario de bloco 
+este tipo de comentario pode ter varias linhas
+*/ 
+
+//declaração de variavel
+$nome="william";
+
+// concanatação e saida de dados
+
+echo "olá " .$nome. "!<br>";
+
+echo "olá mundo !<br>";
+print("olá");
+
+
+
+
+
+?>
