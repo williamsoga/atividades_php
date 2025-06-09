680625b09f8b9bbb2ed14549228bffc3f418c3b8
williamsoga <williamhsoga@gmail.com>
2025-06-09 11:14:35 -0300

    atividade_um

---
+++ teste.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>varaiveis em php</title>
+</head>
+<body>
+    <h1>varaiveis em php</h1>
+    <?php 
+     $valor="william";
+     
+     echo $valor."<br> <br>";
+
+     $valor=9;
+     
+     $valor=$valor+1;
+     
+     $valor +=2;
+
+     echo $valor;
+    ?> 
+
+</body>
+</html>
